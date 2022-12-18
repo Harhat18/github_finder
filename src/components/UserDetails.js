8e60cc4efa4477a5518bb650513462cb985f6c0f
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-12-18 18:05:54 +0300

    Add files via upload

---
+++ src/components/UserDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class UserDetails extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+  render() {
+    return <div>{this.props.user.name}</div>;
+  }
+}
+
+export default UserDetails;
